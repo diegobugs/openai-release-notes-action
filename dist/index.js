330120454e7fb544145ab368466d58ef83d9f2c4
Diego Bugs <diego.f.bugs@gmail.com>
2024-09-19 17:03:09 -0400

    Add dist

--- dist/index.js
+++ dist/index.js
@@ -47138,10 +47138,6 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-/**
- * Entry point of the application
- */
-
 const { getInput, setFailed, info } = __nccwpck_require__(6024);
 const { context, getOctokit } = __nccwpck_require__(5016);
 const OpenAI = __nccwpck_require__(2140);
@@ -47149,9 +47145,11 @@ const OpenAI = __nccwpck_require__(2140);
 /**
  * @typedef {Object} ActionInputs
  * @property {string} language - The language (defaults to "en")
+ * @property {string} [model] - The model (defaults to "gpt-4o")
  * @property {string} openaiApiKey - The OpenAI API key
  * @property {string} version - The release version
  * @property {string} [token] - The token (optional)
+ * @property {string} [useGithubGeneratedNotes] - Use Github generated notes (optional)
  */
 
 /**
@@ -47161,15 +47159,38 @@ const OpenAI = __nccwpck_require__(2140);
 function parseInputs() {
   const openaiApiKey = getInput("openai-api-key");
   const language = getInput("language");
+  const model = getInput("model");
   const token = getInput("token");
   const version = getInput("version");
+  const useGithubGeneratedNotes = getInput("use-github-generated-notes");
 
   return {
     language,
+    model,
     openaiApiKey,
     token,
     version,
+    useGithubGeneratedNotes,
   };
+}
+
+/**
+ * Function to get PRs associated with a commit.
+ * @param {string} octokit - The octokit instance
+ * @param {string} sha - The commit SHA
+ * @returns {Array<{label: string, url: string}[]>} The PRs associated with the commit
+ */
+async function getPRsFromCommit(octokit, sha) {
+  const pr = await octokit.rest.pulls.listPullRequestsAssociatedWithCommit({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    commit_sha: sha,
+  });
+
+  return pr.data.map((p) => ({
+    label: `#${p.number}`,
+    url: p.html_url,
+  }));
 }
 
 /**
@@ -47177,7 +47198,14 @@ function parseInputs() {
  */
 async function run() {
   info("Running ai release notes action");
-  const { openaiApiKey, language, token, version } = parseInputs();
+  const {
+    openaiApiKey,
+    language,
+    model,
+    token,
+    version,
+    useGithubGeneratedNotes,
+  } = parseInputs();
   const octokit = getOctokit(token);
 
   if (context.eventName !== "pull_request") {
@@ -47192,38 +47220,68 @@ async function run() {
     pull_number: prNumber,
   });
 
-  // Read PR merged related commits and PR body
+  if (!commits.data.length) {
+    throw new Error("No commits found in the pull request");
+  }
 
   try {
     // Get the release notes
-    info(`OpenAI key: ${openaiApiKey.slice(0, 4)}...`);
+    info(`OpenAI key: ${openaiApiKey.slice(0, 6)}...`);
     // Call the OpenAI API
     const openai = new OpenAI({
       apiKey: openaiApiKey,
     });
 
+    let githubNotesContext = "";
+    if (useGithubGeneratedNotes) {
+      try {
+        const previousVersion = await octokit.rest.repos.getLatestRelease({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+        });
+        const notes = await octokit.rest.repos.generateReleaseNotes({
+          owner: context.repo.owner,
+          repo: context.repo.repo,
+          tag_name: version,
+          target_commitish: context.sha,
+          previous_tag_name: previousVersion,
+        });
+        githubNotesContext =
+          "\nFor more context use Github auto generated log also as context:\n" +
+          `${JSON.stringify(notes, null, 2)}`;
+      } catch (error) {
+        info("Failed to generate github notes", error);
+      }
+    }
+
     const prompt =
-      "You are a DEV OP engineer, your responsibility is write changelog of the new software version." +
+      "You are a DEV OPS engineer, your responsibility is write changelog of the new software version." +
       "The changelog consist on useful information about the new features and bug fixes of the software." +
       "The changelog must be clear and concise, so the users can understand the changes." +
-      "The changelog must be written in markdown format." +
-      `The changelog must be written in the following language '${language}'.` +
-      "The changelog must use words 'add' for features, changes, improvements, updates and 'fix' for hotfixes, fixes" +
-      "The changelog must be written in the following structure:" +
+      "The changelog must use words 'add' for features, changes, improvements, updates and 'fix' for hot-fixes, bugfix" +
+      "The changelog must be organized with features first and then bug fixes." +
+      "The changelog must be written in the following structure:\n" +
+      "```\n" +
       "## What's Changed" +
-      "- Add new feature by @user" +
-      "- Fix bug by @user" +
-      "Do not ask for more information, use the following information to write the changelog." +
-      "The following information that made in this version (commit message, author):" +
+      "- Add new feature by @user, [#PR number](url)" +
+      "- Fix bug by @user, [#PR number](url)" +
+      (useGithubGeneratedNotes &&
+        "### Full Changelog: [previous version...new version](url)") +
+      "```" +
+      "\nDo not ask for more information." +
+      "\nUse only the following data to write the changelog (commit message, author, PRs):" +
       `${JSON.stringify(
         commits.data.map((c) => ({
           message: c.commit.message,
           author: c.author.name,
           authorUrl: c.author.html_url,
+          prs: getPRsFromCommit(octokit, c.sha),
         })),
         null,
         2
-      )}`;
+      )}` +
+      githubNotesContext +
+      `\nThe changelog must be written in the following language '${language}'. Translate everything to this language.`;
 
     const completion = await openai.chat.completions.create({
       messages: [
@@ -47232,7 +47290,7 @@ async function run() {
           content: prompt,
         },
       ],
-      model: "gpt-4o",
+      model: model || "gpt-4o",
     });
 
     if (completion) {
